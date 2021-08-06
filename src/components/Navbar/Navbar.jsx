8329ff0115dbdeb17d867de2e9e57dd92d8e4ad6
henrytranvn93 <henry.tran@htmi.ch>
2021-08-06 15:18:51 -0700

    add styling navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
-import { Appbar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons'
 
+import logo from '../../assets/ecommercelogo.png'
+import useStyles from './styles'
+
 function Navbar() {
+    const classes = useStyles();
     return (
         <>
-            <Appbar position='fixed' className={classes.appBar} color='inherit'>
+            <AppBar position='fixed' className={classes.appBar} color='inherit'>
                 <Toolbar>
+                    <Typography variant='h6' className={classes.title} color='inherit'>
+                        <img src={logo} alt="Commerce Shop" height="25px" className={classes.image}/>
+                        Commerce Shop
+                    </Typography>
+                    <div className={classes.grow}></div>
+                    <div className={classes.button}>
+                        <IconButton aria-label="Show card itesm" color='inherit'>
+                            <Badge badgeContent={2} color='secondary'>
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>   
+                    </div>
                 </Toolbar>
-            </Appbar>            
+            </AppBar>            
         </>
     )
 }
