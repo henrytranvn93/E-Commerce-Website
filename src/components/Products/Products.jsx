333da81be2a4efdcd4d1febb2609941b9b617458
henrytranvn93 <henry.tran@htmi.ch>
2021-08-06 15:49:26 -0700

    implemented commercejs lib to fetch  products (backend)

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
+
 
 const products = [
     { id: 1, name: 'Shoes', description: 'Running Shoes.', price: '$100', image: 'https://www.pngall.com/wp-content/uploads/2016/04/Running-Shoes-PNG-Pic.png' },
@@ -9,9 +11,11 @@ const products = [
 ]
 
 const Products = () => {
+    const classes = useStyles();
     return (
-        <main>
-            <Grid container justify="center" spacing={4}>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Grid container justifyContent="center" spacing={4}>
                 {products.map(product => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
                         <Product product={product} />
