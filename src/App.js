ae372d83b566bad5ec6f8307b9ec449637a3c765
DESKTOP-G34PGPB\Kieu Tran <henry.tran@htmi.ch>
2021-09-02 18:13:10 -0600

    add cart and AddtoCart function to the site

--- src/App.js
+++ src/App.js
@@ -6,20 +6,33 @@ import { Products, Navbar } from './components';
 
 const App = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
         setProducts(data);
     }
 
+    const fetchCart = async () => {
+        const cart = await commerce.cart.retrieve();
+        setCart(cart);
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+    }
+
     useEffect(() => {
         fetchProducts();
+        fetchCart();
     }, []);
 
     return (
         <div>
             <Navbar />
-            <Products products={products}/>
+            <Products products={products} onAddToCart={handleAddToCart}/>
         </div>
     )
 }
